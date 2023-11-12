62ba3489b9df65ae9dda38192f48b3f96e000c25
Angel <angelogabriel.contact@gmail.com>
2023-11-12 11:46:10 -0300

    Adding constructor and attributes to Vector class

--- src/src/co/datastructure/vetor/Vetor.java
+++ src/src/co/datastructure/vetor/Vetor.java
@@ -1,4 +1,16 @@
 package co.datastructure.vetor;
 
 public class Vetor {
+
+    //Attributes
+    private String[] elements;
+    private int size;
+
+    //Constructor
+    public Vetor(int capacity){
+        elements = new String[capacity];
+        size = 0;
+    }
+
+
 }
